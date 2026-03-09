18c0bfd7b89e9a1481f112d6ca83479550cbf031
Even Gunnes <even@evolo.no>
2026-03-09 14:19:16 +0100

    (fix): remove unused fallback

--- src/lib/services/WriteProperty.ts
+++ src/lib/services/WriteProperty.ts
@@ -497,7 +497,7 @@ export default class WriteProperty extends BacnetService {
 				)
 			}
 			const entry = Array.isArray(values)
-				? values[arrayIndex - 1] || values[0]
+				? values[arrayIndex - 1]
 				: values
 			if (
 				WriteProperty.hasTypeAndValue(entry) &&
